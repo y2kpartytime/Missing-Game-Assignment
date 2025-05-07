d442645efb95cc840034051731b0faffbbc632f0
y2kpartytime <C24732831@myTUDublin.ie>
2025-05-07 12:07:25 +0100

    2
    2

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        if (movement.magnitude > 0.1f)
+        {
+            transform.up     = movement.normalized;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+    }
+}
